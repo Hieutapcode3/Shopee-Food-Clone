ad1b180451ff1838d0687fb4953940458a3ca238
Hieutapcode3 <hieupham9155@gmail.com>
2024-06-11 07:16:13 +0700

    update file

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,24 +1,3 @@
-// Modal
-const get_place = document.querySelector(".js-get-place");
-const modal = document.querySelector(".modal");
-const modal_container = document.querySelector(".modal-container");
-const close = document.querySelector(".js-modal-close");
-function hideGetPlace() {
-  modal_container.classList.add("modal-fadeOut");
-}
-function showGetPlace() {
-  modal.classList.add("open");
-  modal_container.classList.remove("modal-fadeOut");
-}
-get_place.addEventListener("click", showGetPlace);
-close.addEventListener("click", hideGetPlace);
-modal_container.addEventListener("animationend", () => {
-  if (modal_container.classList.contains("modal-fadeOut")) {
-    modal.classList.remove("open");
-    modal_container.classList.remove("modal-fadeOut");
-  }
-});
-
 // <Nav
 const subnav = document.querySelectorAll(".li");
 for (const item of subnav) {
